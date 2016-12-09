685f1c9e76c69bc7d24385fa3b756801b3ffdcff
artyhacker <artyhacker@outlook.com>
2016-12-09 19:53:22 +0800

    repair bug

--- app/src/main/java/com/artyhacker/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/artyhacker/popularmovies/MovieDetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,9 +24,11 @@ public class MovieDetailsActivity extends AppCompatActivity {
     @BindView(R.id.movie_date_tv) TextView movieDate;
     @BindView(R.id.movie_score_tv) TextView movieScore;
     @BindView(R.id.movie_content_tv) TextView movieOverview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_movie_details);
         ButterKnife.bind(this);
 
